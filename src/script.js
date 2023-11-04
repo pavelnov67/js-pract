2169a850eccfdc55868a79b66b159f1e837f4259
pavelnov67 <104889944+pavelnov67@users.noreply.github.com>
2023-11-04 20:16:09 +0300

    first js project
    login page

---
+++ src/script.js
@@ -0,0 +1,43 @@
+document.getElementById('main-form').addEventListener("submit", checkForm);
+
+function checkForm(event) {
+  event.preventDefault();
+  let el = document.getElementById('main-form');
+  let name = el.name.value;
+  let pass = el.pass.value;
+  let repass = el.repass.value;
+  let state = el.state.value;
+
+  console.log(name);
+  console.log(pass);
+  console.log(repass);
+  console.log(state);
+
+  let fail = "";
+
+  if(name.length <3)
+    fail = "Слишком короткое имя";
+
+  else if(name.length >25)
+    fail = "Слишком длинное имя";
+
+  else if(pass.length <3)
+    fail = "Слишком короткий пароль";
+
+  else if(pass != repass)
+    fail = "Пароли не совпадают";
+
+  else  if(name == "" || pass == "" || repass == "" || state == "")
+    fail = "Все поля должны быть заполнены";
+
+  else if(pass.split('&').length > 1)
+    fail = 'Не используйте &';
+
+  if(fail != "") {
+    document.getElementById('error').innerHTML = fail;
+  }
+    else {
+      alert("Данные отправлены на сервер");
+      window.location = 'https://gymkhana-cup.ru/'
+    }
+}
